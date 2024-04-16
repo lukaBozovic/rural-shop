6ead6fa252cf5eed0ba42a9d6a15d3a35ddece27
Luka Božović <luka.bozovic@amplitudo.me>
2024-04-16 20:23:39 +0200

    Add some text to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,10 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    Ulogovani ste!
+                <div class="py-6 px-24 bg-white border-b border-gray-200 w-100">
+                    Dobrodošli u našu aplikaciju koja povezuje Vas, ljubitelje svježih i kvalitetnih poljoprivrednih proizvoda, direktno sa lokalnim proizvođačima. Sa ponosom Vam nudimo širok spektar svježih plodova, sočnog voća, hrskavog povrća i još mnogo toga, direktno iz srca poljoprivrednih gazdinstava. Naša platforma omogućava jednostavnu kupovinu direktno od proizvođača, garantujući svježinu i kvalitet svakog proizvoda. Uživajte u autentičnom ukusu sezone, podržite lokalne zajednice i doprinesite održivom razvoju poljoprivrede. Zajedno gradimo most između Vas i poljoprivrednika, kako bi Vaša iskustva bila nezaboravna i puna svježine i zdravlja. Pridružite nam se u ovoj misiji da stvorimo spoj tradicije, kvaliteta i domaćih proizvoda!
                 </div>
             </div>
         </div>
