e11ab886ff3fae6b93b452787e68fe20a964c6e9
Drazen Vuletic <drazen.vuletic@gmail.com>
2024-07-02 22:45:08 +0200

    Fix Ad edit

--- resources/views/ads/edit.blade.php
+++ resources/views/ads/edit.blade.php
@@ -16,7 +16,7 @@
             <x-splade-input name="address" label="Adresa" placeholder="Adresu" required/>
             <x-splade-input name="price" label="Cijena" placeholder="Unesite cijenu" required/>
             <x-splade-select name="unit_id" label="Jedinica mjere" :options="$units"
-                             placeholder="Odaberite jedinicu mjere" choices relation/>
+                             placeholder="Odaberite jedinicu mjere" choices/>
             <x-splade-select name="categories[]" label="Kategorije" :options="$categories"
                              placeholder="Odaberite kategorije" choices multiple relation/>
 
